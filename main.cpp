a26f9799c5ce10c7cfc31fc655d6264b2a31f7ae
Vinicius Oliveira <vinnielauter@gmail.com>
2025-10-29 15:39:52 -0300

    feat: interatividade nas operações da main (ainda incompleto)

--- main.cpp
+++ main.cpp
@@ -7,18 +7,29 @@ int main() {
     ContaCorrente c1(101, "Tiago Mattos", 1000.0, 15.0);
     ContaPoupanca p1(202, "Maria Silva", 5000.0, 0.02); // 2% de rendimento
 
-    cout << "\n=== Conta Corrente ===\n";
-    c1.exibir();
-    c1.depositar(200);
-    c1.sacar(300);
-    c1.cobrarTaxaMensal();
-    c1.exibir();
+    int num_op;
+    double val_op;
 
-    cout << "\n=== Conta Poupança ===\n";
-    p1.exibir();
-    p1.renderJuros();
-    p1.sacar(600);
-    p1.exibir();
+    cout << "\n=== Conta Corrente ===\n";
+    cout << "Selecione a operação que deseja realizar digitando o número correspondente:\n";
+    cout << "(1) Exibir Saldo\n";
+    cout << "(2) Depositar\n";
+    cout << "(3) Sacar\n";
+    cin >> num_op;
+
+    if (num_op == 1) {
+        c1.exibir();
+    }
+    else if (num_op == 2) {
+        cout << "Digite o valor que deseja depositar: ";
+        cin>>val_op;
+        c1.depositar(val_op);
+    }
+    else if (num_op == 3) {
+        cout << "Digite o valor que deseja Sacar: ";
+        cin>>val_op;
+        c1.sacar(val_op);
+    }
 
     return 0;
 }
